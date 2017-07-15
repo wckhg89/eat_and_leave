c43b509502dd0f690e4c3de051b64e28f642304c
kanghonggu <wckhg89@gmail.com>
2017-07-15 18:21:51 +0900

    user domain test code & password encrypt

---
+++ src/main/java/com/guppy/util/SecurityUtil.java
@@ -0,0 +1,7 @@
+package com.guppy.util;
+
+/**
+ * Created by kanghonggu on 2017. 7. 15..
+ */
+public class SecurityUtil {
+}
--- src/test/java/com/guppy/domain/UserTest.java
+++ src/test/java/com/guppy/domain/UserTest.java
@@ -1,7 +1,85 @@
 package com.guppy.domain;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.guppy.repository.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Created by kanghonggu on 2017. 7. 14..
  */
+
+@Transactional
+@Rollback
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserTest {
+
+    private static Logger logger = LoggerFactory.getLogger(UserTest.class);
+
+    @Test
+    public void 사용자_로그인시_초기_User_도메인_초기화 () {
+        Authentication auth = getAuthentication();
+
+        User loginUser = User.initUser(auth);
+
+        logger.info("loginUser Info - {}", loginUser);
+    }
+
+    private Authentication getAuthentication() {
+        return new Authentication() {
+                @Override
+                public Collection<? extends GrantedAuthority> getAuthorities() {
+                    return null;
+                }
+
+                @Override
+                public Object getCredentials() {
+                    return null;
+                }
+
+                @Override
+                public Object getDetails() {
+                    Map<String,String> detailMap =
+                            Maps.newHashMap(ImmutableMap.<String, String>builder()
+                            .put("name", "Test User")
+                            .put("id", "12345").build());
+
+                    return detailMap;
+                }
+
+                @Override
+                public Object getPrincipal() {
+                    return null;
+                }
+
+                @Override
+                public boolean isAuthenticated() {
+                    return false;
+                }
+
+                @Override
+                public void setAuthenticated(boolean b) throws IllegalArgumentException {
+
+                }
+
+                @Override
+                public String getName() {
+                    return null;
+                }
+            };
+    }
+
 }
